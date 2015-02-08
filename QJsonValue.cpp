8e954c0bfc06f879d91f83319b09433a40e594fe
Evan Teran <evan.teran@gmail.com>
2015-02-08 00:09:24 -0500

    adding explicit include, hopefully to fix the build issue Bruno Magne reported

--- QJsonValue.cpp
+++ QJsonValue.cpp
@@ -21,6 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "QJsonObject.h"
 
 #if QT_VERSION < 0x050000
+#include <QtCore/QtAlgorithms>
 
 //------------------------------------------------------------------------------
 // Name: QJsonValue
